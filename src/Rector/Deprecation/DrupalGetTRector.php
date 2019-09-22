38bff425ac1ce4de2414b957cdc77aa49a4233b8
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-09-22 18:39:29 +0530

    D7TC-2: Fixing arguments duplication issue.

--- src/Rector/Deprecation/DrupalGetTRector.php
+++ src/Rector/Deprecation/DrupalGetTRector.php
@@ -5,6 +5,7 @@ namespace Drupal8Rector\Rector\Deprecation;
 use PhpParser\Node;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\RectorDefinition;
+use PhpParser\NodeDumper;
 
 /**
  * Replaces deprecated get_t() calls.
@@ -21,6 +22,9 @@ final class DrupalGetTRector extends AbstractRector
       Node\Expr\Variable::class,
       Node\Expr\Assign::class,
       Node\Stmt\Function_::class,
+      Node\Stmt\Use_::class,
+      Node\Stmt\UseUse::class,
+      Node\Stmt\Nop::class,
     ];
   }
 
@@ -33,16 +37,21 @@ final class DrupalGetTRector extends AbstractRector
       foreach ($node->stmts as $key => $stmt) {
         if ($stmt instanceof Node\Stmt\Expression) {
           if ($stmt->expr->expr->name instanceof Node\Name\FullyQualified && $stmt->expr->expr->name->parts[0] === 'get_t') {
-            $expr_var = $stmt->expr->var->name;
+            // Storing in array so that any variable occurance
+            // in code is replaced.
+            $expr_var[] = $stmt->expr->var->name;
             unset($node->stmts[$key]);
           }
-          if ($stmt->expr->expr->name instanceof Node\Expr\Variable && $stmt->expr->expr->name->name == $expr_var) {
+          if ($stmt->expr->expr->name instanceof Node\Expr\Variable &&  in_array($stmt->expr->expr->name->name, $expr_var)) {
             foreach ($stmt->expr->expr->args as $arg) {
               $t_args[] = $arg;
             }
+
             $t_call = new Node\Expr\FuncCall(new Node\Name('t'), $t_args);
             $changed_statement = new Node\Stmt\Expression($t_call);
             $node->stmts[$key] = $changed_statement;
+            // Unset arguments array.
+            unset($t_args);
           }
         }
       }
