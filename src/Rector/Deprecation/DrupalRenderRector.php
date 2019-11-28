7bb99b0a68140e33f4c6d468e90511d9e8c45835
David Czinege <czinege.david.89@gmail.com>
2019-11-28 18:35:24 +0100

    The drupal_render_root() function fix is added to the DrupalRenderRector.

--- src/Rector/Deprecation/DrupalRenderRector.php
+++ src/Rector/Deprecation/DrupalRenderRector.php
@@ -7,7 +7,7 @@ use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\RectorDefinition;
 
 /**
- * Replaces deprecated drupal_render() calls.
+ * Replaces deprecated drupal_render() and drupal_render_root() calls.
  */
 final class DrupalRenderRector extends AbstractRector
 {
@@ -27,10 +27,14 @@ final class DrupalRenderRector extends AbstractRector
     {
         /** @var Node\Expr\FuncCall $node */
         // Ignore those complex cases when function name specified by a variable.
-        if ($node->name instanceof Node\Name && 'drupal_render' === (string) $node->name) {
+        if ($node->name instanceof Node\Name && in_array((string) $node->name, ['drupal_render', 'drupal_render_root'])) {
             // Use this format \Drupal::service('renderer')->render($elements, $is_recursive_call).
             $renderer_service = new Node\Expr\StaticCall(new Node\Name\FullyQualified('Drupal'), 'service', [new Node\Scalar\String_('renderer')]);
-            $node = new Node\Expr\MethodCall($renderer_service, new Node\Identifier('render'), $node->args);
+            $function_method_name_mapping = [
+                'drupal_render' => 'render',
+                'drupal_render_root' => 'renderRoot',
+            ];
+            $node = new Node\Expr\MethodCall($renderer_service, new Node\Identifier($function_method_name_mapping[(string) $node->name]), $node->args);
         }
         return $node;
     }
@@ -39,6 +43,6 @@ final class DrupalRenderRector extends AbstractRector
      */
     public function getDefinition(): RectorDefinition
     {
-        return new RectorDefinition(sprintf('Fixes deprecated drupal_render() calls'));
+        return new RectorDefinition(sprintf('Fixes deprecated drupal_render() and drupal_render_root() calls'));
     }
 }
